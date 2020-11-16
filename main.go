0ef6d306952b9d9374c22379a5274d9a3e432b98
oyugirachel <oyugirachel20@gmail.com>
2020-11-16 14:07:15 +0300

    adding functionality to the routes

--- main.go
+++ main.go
@@ -2,10 +2,40 @@ package main
 
 // Import our dependencies. We'll use the standard HTTP library as well as the gorilla router for this app
 import (
-
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
 )
+type Product struct {
+	Id int
+	Name string
+	Slug string
+	Description string
+}
+
+/* We will create our catalog of VR experiences and store them in a slice. */
+var products = []Product{
+	Product{Id: 1, Name: "World of Authcraft", Slug: "world-of-authcraft", Description : "Battle bugs and protect yourself from invaders while you explore a scary world with no security"},
+	Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description : "Explore the depths of the sea in this one of a kind underwater experience"},
+	Product{Id: 3, Name: "Dinosaur Park", Slug : "dinosaur-park", Description : "Go back 65 million years in the past and ride a T-Rex"},
+	Product{Id: 4, Name: "Cars VR", Slug : "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
+	Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description : "Pick up the bow and arrow and master the art of archery"},
+	Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description : "Explore the seven wonders of the world in VR"}}
+/* The status handler will be invoked when the user calls the /status route
+   It will simply return a string with the message "API is up and running" */
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("API is up and running"))
+})
+
+/* The products handler will be called when the user makes a GET request to the /products endpoint.
+   This handler will return a list of products available for users to review */
+var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	// Here we are converting the slice of products to JSON
+	payload, _ := json.Marshal(products)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
+})
 
 func main() {
 	// Here we are instantiating the gorilla/mux router
